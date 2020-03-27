a45ed553258d87b8d37ae77dd251756e28f20d3b
TopMAXIM <58586134+TopMAXIM@users.noreply.github.com>
2020-03-27 13:11:32 +0900

    Add files via upload

--- 201624010최종민_gugu.java
+++ 201624010최종민_gugu.java
@@ -1,1 +1,32 @@
-# 2020_Report02
+import java.util.Scanner;
+
+public class gugu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a,b;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("구구단 프로그램입니다.");
+		System.out.print("단을 입력해주세요 : ");
+		
+		a = input.nextInt();
+		
+		System.out.println();
+		System.out.println("=-=-=-=-=-=-=-=");
+		
+		for(int i=1;i<=9;i++) {
+			b = a*i;
+			System.out.println(a+" X "+i+" = "+b);
+		}
+		
+		System.out.println("=-=-=-=-=-=-=-=");
+		System.out.println();
+		System.out.println("구구단 종료.");
+		
+		
+	}
+
+}
